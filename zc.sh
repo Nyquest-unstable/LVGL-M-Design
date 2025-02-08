49c86a5198469c90b41e010adbab8691bdb541d6
Nyquest-unstable <2721426054@qq.com>
2025-02-07 17:25:11 -0800

    重新设置setup cmake

--- zc.sh
+++ zc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-cmake -B build -S .
+cmake -DCMAKE_TOOLCHAIN_FILE=./user_cross_compile_setup.cmake -B build -S .
+# cmake -B build -S .
 make -C build -j
 
 cp bin/lvglsim ~/NetFloder/nfs/root/root/
